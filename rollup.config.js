4c67b25c6fc420f10dfef0be092e7571b9c34022
Chan Leung <chan.leung@submittable.com>
2022-09-13 11:27:19 -0700

    chore: update setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
@@ -16,14 +17,22 @@ export default {
             sourcemap: true
         }
     ],
+    external: ['react', 'prop-types', '@mui/material'],
     plugins: [
+        resolve(),
+        json(),
         peerDepsExternal(),
         postcss({
-            extract: false,
+            extract: true,
+            namedExports: true,
             modules: true,
             use: ['less'],
         }),
-        babel({babelHelpers: 'bundled'}),
+        babel({
+            exclude: "node_modules/**",
+            presets: ["@babel/preset-react"],
+            babelHelpers: 'bundled'
+        }),
         resolve(),
         commonjs(),
         terser()
